0bc5177023e84f8a93c1efa472f46351b70ab5be
Victor Pereira <victor.pereira@adyton.net>
2012-03-21 17:43:16 +0100

    check if user is root

--- lib/jiboia/dumpcap.rb
+++ lib/jiboia/dumpcap.rb
@@ -5,6 +5,12 @@ module Jiboia
       #define it as config options
       #check if we are root
       def run(args = {:device=>"eth0",:fname=>"packets.cap"})
+        
+        if Process.uid != 0
+          puts "You must be root"
+          Process.exit false
+        end
+        
         pid = fork do
           Dir.chdir(Jiboia::root_dir)
           exec Jiboia::dumpcap, "-i #{args[:device]} -b duration:3600 -b files:25 -w #{args[:fname]}"
