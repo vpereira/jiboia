872b0c22b4f814e76f2135b9a0a09acabcf87bed
Victor Pereira <victor.pereira@adyton.net>
2012-03-23 17:23:05 +0100

    added path to Dumpcap bin

--- lib/jiboia/dumpcap.rb
+++ lib/jiboia/dumpcap.rb
@@ -14,11 +14,11 @@ module Jiboia
           Process.exit false
         end
         
-        puts "#{Jiboia::dumpcap} -i #{args[:device]} -b duration:3600 -b files:25 -w #{args[:fname]} -p"
+        #puts "#{Jiboia::dumpcap} -i #{args[:device]} -b duration:3600 -b files:25 -w #{args[:fname]} -p"
         pid = fork do
           Dir.chdir(Jiboia::root_dir)
           #we dont put the interface in promiscuous mode and we set the silent mode
-          Process.exec Jiboia::dumpcap, "-i", "#{args[:device]}", "-b", "duration:3600", "-b","files:25","-w","#{args[:fname]}" ,"-p"
+          Process.exec Jiboia::dumpcap, "-i", "#{args[:device]}", "-b", "duration:3600", "-b","files:25","-w","#{File.join(Jiboia.root_dir,args[:fname])}" ,"-p"
         end
         #Detach it and let the process free as a bird 
         Process.detach(pid)
